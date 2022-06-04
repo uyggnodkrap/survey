89079c9102aa013881314963efddc74ab2084d1b
uyggnodkrap <uyggnodkrap26@gmail.com>
2022-06-04 14:30:26 +0900

    BUILD : add class
    repository, model, controller 클래스 추가했습니다.

---
+++ src/main/java/com/survey/model/Survey.java
@@ -0,0 +1,7 @@
+package com.survey.model;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Survey {
+}
---
+++ src/main/java/com/survey/repository/SurveyRepository.java
@@ -0,0 +1,7 @@
+package com.survey.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class SurveyRepository {
+}
