a98e9c18bd5fa67189d6540ee52651b90e3af138
sumin <dltnals9957@gmail.com>
2022-06-04 16:05:52 +0900

    question

--- src/main/java/com/survey/model/Survey.java
+++ src/main/java/com/survey/model/Survey.java
@@ -1,16 +1,7 @@
 package com.survey.model;
 
-import java.time.LocalDate;
+import javax.persistence.Entity;
 
-
+@Entity
 public class Survey {
-
-    private Long survey_id;
-    private int user_id;
-    private String survey_name;
-    private String publish_state;
-    private int required;
-    private LocalDate publish_start;
-    private LocalDate publish_end;
-
 }
---
+++ src/main/java/com/survey/model/question.java
@@ -0,0 +1,9 @@
+package com.survey.model;
+
+import javax.persistence.Id;
+
+public class question {
+    private Long id;
+    private int questingId;
+    private String questionContent;
+}
