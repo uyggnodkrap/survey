de89eaa50bb3fb7fb82a6561e0b0f38fb4836df9
uyggnodkrap <uyggnodkrap26@gmail.com>
2022-06-04 16:15:47 +0900

    FEAT : add survey entity

--- src/main/java/com/survey/model/Survey.java
+++ src/main/java/com/survey/model/Survey.java
@@ -4,4 +4,14 @@ import javax.persistence.Entity;
 
 @Entity
 public class Survey {
+
+
+    private Long survey_id;
+    private int user_id;
+    private String survey_name;
+    private String publish_state;
+    private int required;
+    private LocalDate publish_start;
+    private LocalDate publish_end;
+
 }
